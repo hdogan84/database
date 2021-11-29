b96e406a156a0a107dc4fcd7e1aea9ac0022c099
benjamin.werner <benjamin.werner@mfn.berlin>
2021-11-30 00:42:43 +0100

    add xeno canto crawler and import script

---
+++ src/crawler/crawl-xeno-canto.py
@@ -0,0 +1,156 @@
+import requests
+
+from datetime import datetime
+import shutil
+from pathlib import Path
+import csv
+
+species_list = [
+    "Fringilla coelebs",
+    "Scolopax rusticola",
+    "Crex crex",
+    "Chloris chloris",
+    "Coccothraustes coccothraustes",
+    "Spinus spinus",
+    "Anthus trivialis",
+    "Erithacus rubecula",
+    "Ficedula hypoleuca",
+    "Muscicapa striata",
+    "Phoenicurus phoenicurus",
+    "Cyanistes caeruleus",
+    "Lophophanes cristatus",
+    "Parus major",
+    "Periparus ater",
+    "Poecile palustris",
+    "Phylloscopus sibilatrix",
+    "Phylloscopus trochilus",
+    "Sitta europaea",
+    "Sylvia atricapilla",
+    "Troglodytes troglodytes",
+    "Turdus merula",
+    "Turdus philomelos",
+    "Turdus viscivorus",
+    "Dendrocopos major",
+]
+
+download_path = "./downloads"
+collection = "xeno_canto-ammod"
+result_file = "collection-data.csv"
+result_filepath = download_path + "/" + collection + "/" + result_file
+
+end_date = datetime.strptime("2020-05-01", "%Y-%m-%d")
+
+
+def write_to_csv(data, filename, header) -> None:
+    with open(filename, "w", newline="") as csvfile:
+        csv_writer = csv.writer(
+            csvfile, delimiter=";", quotechar="|", quoting=csv.QUOTE_MINIMAL
+        )
+        if header is not None:
+            csv_writer.writerow(header)
+        csv_writer.writerows(data)
+
+
+def download_file(url, local_filename, subfolder=None):
+    print("Start download " + url + " " + local_filename)
+    download_folder = (
+        download_path + "/" + collection + (subfolder if subfolder is not None else "")
+    )
+    Path(download_folder).mkdir(parents=True, exist_ok=True)
+    print("Download file: " + local_filename)
+    with requests.get(url, stream=True) as r:
+        with open(download_folder + "/" + local_filename, "wb") as f:
+            shutil.copyfileobj(r.raw, f)
+    return local_filename
+
+
+def duration_string_to_seconds(duration):
+    tmp = duration.split(":")
+    if len(tmp) is 2:
+        return int(tmp[0]) * 60 + int(tmp[1])
+    if len(tmp) is 3:
+        return int(tmp[0]) * 60 * 60 + int(tmp[1]) * 60 + int(tmp[2])
+
+
+def make_api_call(species, page=None):
+    r = requests.get(
+        "https://www.xeno-canto.org/api/2/recordings?query={}&dir=1&order=dt".format(
+            species
+        )
+        + ("&page={}".format(page) if page is not None else "")
+    )
+    if r.status_code is 200:
+        return r.json()
+    else:
+        print(
+            "Warning reuest {}  on page {} returns {}".format(
+                species, page, r.status_code
+            )
+        )
+        return None
+
+
+download_till_date = ""
+csv_entries = []
+for species in species_list:
+    result = make_api_call(species)
+    print(result["numRecordings"])
+    print(
+        "Found {} recordings for {} with num species".format(
+            result["numRecordings"], species, result["numSpecies"]
+        )
+    )
+    for page in range(1, result["numPages"]):
+        print(page)
+        result = make_api_call(species, page=page)
+        break_page_iteration = False
+        for index, record in enumerate(result["recordings"]):
+            upload_date = datetime.strptime(record["uploaded"], "%Y-%m-%d")
+
+            if upload_date < end_date:
+                print("Reached end date on page {} record {}".format(page, index))
+                break_page_iteration = True
+                break
+            filepath = download_file("https:" + record["file"], record["file-name"])
+            record_csv_entry = [
+                record["rec"].replace(";", ","),  # recordist sanitize ;
+                record["loc"].replace(";", ","),  # location sanitize ;
+                record["lat"],  # latitude
+                record["lng"],  # longitude
+                record["alt"],  # altitude
+                record["file-name"],  # original_filename
+                record["id"],  # xeno_canto_id
+                record["type"].replace(";", ","),  # call_type
+                record["date"],  # date
+                record["time"],  # time
+                duration_string_to_seconds(record["length"]),  # duration
+            ]
+            species_csv_entry = [species, False]  # species  # isBackground
+            csv_entries.append(record_csv_entry + species_csv_entry)
+            for background_species in record["also"]:
+                if background_species == "":
+                    continue
+                species_csv_entry = [
+                    background_species,  # species
+                    True,  # isBackground
+                ]
+                csv_entries.append(record_csv_entry + species_csv_entry)
+
+        if break_page_iteration:
+            break
+        #     print(record["uploaded"])
+    headline = [
+        "recordist",
+        "location_name",
+        "latitude",
+        "longitude",
+        "altitude",
+        "original_filename",
+        "xeno_canto_id",
+        "call_type",
+        "date",
+        "time",
+        "species",
+        "background",
+    ]
+    write_to_csv(csv_entries, result_filepath, headline)
---
+++ src/import_scripts/import_crawling_results.py
@@ -0,0 +1,190 @@
+import csv
+import pandas
+from pathlib import Path
+from typing import Dict, List
+from datetime import timedelta
+from mysql.connector.cursor import MySQLCursor
+from tools.file_handling.collect import rename_and_copy_to
+from tools.file_handling.audio import read_parameters_from_audio_file
+from tools.configuration import parse_config
+from tools.logging import debug, info, error
+from tools.db.queries import get_id_of_entry_in_table
+from tools.db import (
+    get_entry_id_or_create_it,
+    connectToDB,
+    delete_from_table,
+)
+from tools.logging import info, progbar
+import argparse
+
+TARGET_DATA_PATH = "./tmp"
+CONFIG_FILEPATH = "/home/tsa/projects/libro-animalis/src/config/config_crawled.cfg"
+DATA_PATH = "/home/tsa/projects/libro-animalis/downloads/xeno_canto-ammod"
+CSV_FILEPATH = (
+    "/home/tsa/projects/libro-animalis/downloads/xeno_canto-ammod/collection-data.csv"
+)
+COLLECTION_NAME = "xeno canta 2020-2021 ammod set"
+DRY_RUN = False
+# recordist;location_name;latitude;longitude;altitude;original_filename;xeno_canto_id;call_type;date;time;species;background
+def import_collection(
+    data_path, config_path, csv_filepath, collection_name, dry_run=True
+):
+    config = parse_config(config_path)
+    print(config_path)
+    dataframe = pandas.read_csv(csv_filepath, sep=";", quotechar="|", index_col=False)
+
+    print(config.database.host)
+    print(config.database.port)
+    print(config.database.user)
+    print(config.database.password)
+    print(config.database.name)
+    info("Start importing files")
+    with connectToDB(config.database) as db_connection:
+        print("Connected")
+        with db_connection.cursor() as db_cursor:
+            db_cursor: MySQLCursor
+
+            collection_entry = [("name", collection_name), ("remarks", None)]
+            collection_id = get_entry_id_or_create_it(
+                db_cursor, "collection", collection_entry, collection_entry
+            )
+            counter = 0
+            failed_annotations = []
+            row_count = dataframe.shape[0]
+            for _, row in dataframe.iterrows():
+                progbar(counter, row_count, 20)
+                counter = counter + 1
+                if counter % 1000 == 0:
+                    if dry_run is False:
+                        db_connection.commit()
+
+                species_id = None
+
+                filepath = data_path.joinpath(str(row.loc["original_filename"]))
+
+                if filepath.exists() is False:
+                    error("File does not exist {}".format(filepath.as_posix()))
+                    continue
+                audio_file_parameters = None
+                try:
+                    audio_file_parameters = read_parameters_from_audio_file(filepath)
+                except Exception as e:
+                    error("Could not read audio Parameters from {}".format(filepath))
+                    continue
+
+                target_record_file_path = "{}/{}/{}".format(
+                    audio_file_parameters.md5sum[0],
+                    audio_file_parameters.md5sum[1],
+                    audio_file_parameters.md5sum[2],
+                )
+                record_entry = [
+                    ("duration", audio_file_parameters.duration),
+                    ("sample_rate", audio_file_parameters.sample_rate),
+                    ("bit_depth", audio_file_parameters.bit_depth),
+                    ("bit_rate", audio_file_parameters.bit_rate),
+                    ("channels", audio_file_parameters.channels),
+                    ("mime_type", audio_file_parameters.mime_type),
+                    (
+                        "original_filename",
+                        audio_file_parameters.original_filename,
+                    ),
+                    ("file_path", target_record_file_path),
+                    ("filename", audio_file_parameters.filename),
+                    ("md5sum", audio_file_parameters.md5sum),
+                    ("collection_id", collection_id),
+                ]
+
+                (record_id, created) = get_entry_id_or_create_it(
+                    db_cursor,
+                    "record",
+                    [
+                        ("md5sum", audio_file_parameters.md5sum),
+                    ],
+                    data=record_entry,
+                    info=True,
+                )
+                if created:
+                    # move file to destination
+                    if dry_run is False:
+                        targetDirectory = (
+                            config.database.get_originals_files_path().joinpath(
+                                target_record_file_path
+                            )
+                        )
+                        targetDirectory.mkdir(parents=True, exist_ok=True)
+                        rename_and_copy_to(
+                            filepath,
+                            targetDirectory,
+                            audio_file_parameters.filename,
+                        )
+
+                species_id = get_id_of_entry_in_table(
+                    db_cursor, "species", [("latin_name", row.loc["species"])]
+                )
+
+                if species_id is None:
+                    failed_annotations.append(
+                        (row.loc["species"], audio_file_parameters.original_filename)
+                    )
+                    print("failed _annoation species id not found")
+                    continue
+                # create foreground annoation
+                annoation_data = [
+                    ("record_id", record_id),
+                    ("start_time", 0),
+                    ("end_time", audio_file_parameters.duration),
+                    ("species_id", species_id),
+                    ("background", bool(row.loc["background"])),
+                ]
+                get_entry_id_or_create_it(
+                    db_cursor,
+                    "annotation_of_species",
+                    query=annoation_data,
+                    data=annoation_data,
+                )
+                if dry_run is False:
+                    db_connection.commit()
+
+            print(failed_annotations)
+
+
+parser = argparse.ArgumentParser(description="")
+parser.add_argument(
+    "--data_path",
+    metavar="path",
+    type=Path,
+    nargs="?",
+    help="target folder",
+    default=DATA_PATH,
+)
+parser.add_argument(
+    "--csv_path",
+    metavar="path",
+    type=Path,
+    nargs="?",
+    help="csv file with all entries",
+    default=CSV_FILEPATH,
+)
+parser.add_argument(
+    "--config",
+    metavar="path",
+    type=Path,
+    nargs="?",
+    default=CONFIG_FILEPATH,
+    help="config file with database credentials",
+)
+
+parser.add_argument(
+    "--collection",
+    metavar="path",
+    type=str,
+    nargs="?",
+    default=COLLECTION_NAME,
+    help="config file with database credentials",
+)
+
+args = parser.parse_args()
+if __name__ == "__main__":
+    import_collection(
+        args.data_path, args.config, args.csv_path, args.collection, dry_run=DRY_RUN
+    )
