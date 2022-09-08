9b565e2ca217f06eb906255d712e6b7a01bd9cb2
benjamin.werner <benjamin.werner@mfn.berlin>
2022-09-08 16:40:30 +0200

    update impoert script olaf for new db structure

--- src/import_scripts/import_annoations_olaf.py
+++ src/import_scripts/import_annoations_olaf.py
@@ -83,17 +83,8 @@ def import_data(data_path=None, config_file_path=None) -> List[str]:
                         else None,
                     ),
                     (
-                        "start",
+                        "time",
                         file_name_infos.record_datetime.time()
-                        if file_name_infos.record_datetime is not None
-                        else None,
-                    ),
-                    (
-                        "end",
-                        (
-                            file_name_infos.record_datetime
-                            + timedelta(seconds=ceil(file_parameters.duration))
-                        ).time()
                         if file_name_infos.record_datetime is not None
                         else None,
                     ),
@@ -184,7 +175,7 @@ def import_data(data_path=None, config_file_path=None) -> List[str]:
                         ("quality_tag", a.quality_tag),
                         ("id_level", a.id_level),
                         (
-                            "channel",
+                            "channel_ix",
                             a.best_channel if a.best_channel is not None else a.channel,
                         ),  # Here best channel is used as channel of annotation
                         ("start_time", a.start_time),
@@ -195,8 +186,8 @@ def import_data(data_path=None, config_file_path=None) -> List[str]:
                         ("annotation_interval_id", interval_id),
                     ]
 
-                    print(annoation_data)
-                    print("------")
+                    #print(annoation_data)
+                    #print("------")
                     get_entry_id_or_create_it(
                         db_cursor,
                         "annotation_of_species",
